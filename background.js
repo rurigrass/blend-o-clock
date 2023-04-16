7856178825f17e4cc4ce3d05fc6560d9668c3e26
rurigrass <ruripg@gmail.com>
2023-04-16 23:22:50 +0200

    tab listener added

--- background.js
+++ background.js
@@ -1,1 +1,13 @@
-const background = ""
+// const background = ""
+
+console.log("I am background");
+
+// chrome.action.onClicked.addListener(tab => { 
+//     console.log(tab);
+//     const {url} = tab;
+//     console.log(`Loading: ${url}`); 
+//   });
+
+chrome.tabs.onCreated.addListener(function(tab) {
+    console.log(tab);
+})
